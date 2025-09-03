1f3e4c2e3c62f8e2af7a53cae4d2fda2ee90c02a
Ivan <valicentiivan@gmail.com>
2025-09-03 20:49:03 -0300

    subida 2

--- entrega 1/parte 1.py
+++ entrega 1/parte 1.py
@@ -1,1 +1,21 @@
-"hola"
+# apartado de clases
+
+class Usuario:
+    def __init__(self, nombre, email):
+        self._nombre = nombre
+        self._email = email
+        
+
+class Servidorcorreo:
+    def __init__(self, dominio):
+        self._dominio = dominio
+        self._usuarios = []
+
+    def agregar_usuario(self, usuario):
+        self._usuarios.append(usuario)
+
+    def eliminar_usuario(self, usuario):
+        self._usuarios.remove(usuario)
+
+    def obtener_usuarios(self):
+        return self._usuarios
