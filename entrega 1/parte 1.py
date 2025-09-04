4368ddcbfabe8524b71d42510d349ee8dc9841b3
Ivan <valicentiivan@gmail.com>
2025-09-03 21:02:04 -0300

    subida

--- entrega 1/parte 1.py
+++ entrega 1/parte 1.py
@@ -1,14 +1,21 @@
 # apartado de clases
 
 class Usuario:
-    def __init__(self, nombre, email):
+    def __init__(self, nombre,email, servidor,carpetas):
         self._nombre = nombre
         self._email = email
-        
+        self._servidor = servidor
+        self._carpetas = carpetas # diccionario de carpetas
+        self._carpetas = {
+            "inbox": carpetas("inbox"),
+            "enviados": carpetas("enviados"),
+            "papelera": carpetas("papelera")
+        }
+
 
 class Servidorcorreo:
-    def __init__(self, dominio):
-        self._dominio = dominio
+    def __init__(self, email):
+        self._email = email
         self._usuarios = []
 
     def agregar_usuario(self, usuario):
