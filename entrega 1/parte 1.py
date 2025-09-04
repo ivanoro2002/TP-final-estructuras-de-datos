ca9d72dce60a795a5537a9c09fe3ba84019ba761
Ivan <valicentiivan@gmail.com>
2025-09-04 19:11:50 -0300

    subida jueves

--- entrega 1/parte 1.py
+++ entrega 1/parte 1.py
@@ -12,8 +12,37 @@ class Usuario:
             "papelera": carpetas("papelera")
         }
 
+class Mensaje:
+    def __init__(self, remitente, destinatario, asunto, contenido, fecha):
+        self._remitente = remitente
+        self._destinatario = destinatario
+        self._asunto = asunto
+        self._contenido = contenido
+        self._fecha = fecha
+        self._leido = False
 
-class Servidorcorreo:
+    def marcar_como_leido(self):
+        self._leido = True
+        
+    def marcar_como_no_leido(self):
+        self._leido = False
+
+    
+class Carpeta:
+    def __init__(self,nombre):
+        self._nombre = nombre
+        self._mensajes = []
+
+    def agregar_mensaje(self, mensaje):
+        self._mensajes.append(mensaje)
+
+    def eliminar_mensajes(self,mensaje):
+        self._mensajes.remove(mensaje)
+
+    def listar_mensajes(self):
+        return self._mensajes
+
+class ServidorCorreo:
     def __init__(self, email):
         self._email = email
         self._usuarios = []
