fbe629613ed7bf31d5743b1125a57924cb5cf8b4
Ivan <valicentiivan@gmail.com>
2025-09-13 20:33:31 -0300

    subida correccion errores

--- entrega 1/parte 1.py
+++ entrega 1/parte 1.py
@@ -1,61 +1,83 @@
-# apartado de clases
+from abc import ABC, abstractmethod
 
-class Usuario:
+class GestionCorreo(ABC):
+    @abstractmethod
+    def enviar_msjs(self, destinarios: str, asunto: str, contenido: str):
+        pass
+
+    @abstractmethod    
+    def recibir_msjs(self):
+        pass
+
+    @abstractmethod
+    def listar_msjs(self, carpeta: str):
+        pass
+
+class ServidorCorreo:  #Representa el servidor de mensajeria
+    def __init__(self, email): # Construye la estructura.
+        self._email = email # Registra el email del usuario.
+        self._usuarios = [] # Lista de usuarios en la que se guardaran los mismos.
+
+    def agregar_usuario(self, usuario): # Agrega usuarios al servidsdor.
+        self._usuarios.append(usuario)
+
+    def eliminar_usuario(self, usuario):
+        self._usuarios.remove(usuario) # Elimina usuarios del servidor.
+
+    def obtener_usuarios(self):
+        return self._usuarios  # Devuelve la lista de usuarios total en el servidor.
+
+class Usuario(GestionCorreo):
     def __init__(self, nombre,email, servidor,carpetas):
         self._nombre = nombre
         self._email = email
         self._servidor = servidor
-        self._carpetas = carpetas # diccionario de carpetas
         self._carpetas = {
-            "inbox": carpetas("inbox"),
-            "enviados": carpetas("enviados"),
-            "papelera": carpetas("papelera")
-        }
+            "inbox": Carpeta("inbox"),    # Asignación de nombre a 
+            "enviados": Carpeta("enviados"),  # Cada Carpeta
+            "papelera": Carpeta("papelera")
+        }    
 
-class Mensaje:
-    def __init__(self, remitente, destinatario, asunto, contenido, fecha):
-        self._remitente = remitente
-        self._destinatario = destinatario
+class Mensaje: # Sea crea el mensaje 
+    def __init__(self, emisor, destinatarios, asunto, contenido):
+        self._emisor = emisor
+        self._destinatarios = destinatarios
         self._asunto = asunto
-        self._contenido = contenido
-        self._fecha = fecha
-        self._leido = False
-
-    def marcar_como_leido(self):
-        self._leido = True
-        
-    def marcar_como_no_leido(self):
-        self._leido = False
-
+        self._contenido = contenido    
     
-    def mostrar(self):
-        estado = "Leído" if self._leido else "No leído"
-        print(f"Asunto: {self._asunto}\nDe: {self._remitente}\nPara: {self._destinatario}\nFecha: {self._fecha}\nEstado: {estado}\nContenido:\n{self._contenido}")
+    @property
+    def nombre(self):
+        return self._nombre
+    
+    @property
+    def emisor(self):
+        return self._emisor
+        
+    @property
+    def destinarios(self):
+        return self._destinarios
+    
+    @property
+    def asunto(self):
+        return self._asuntos
+    
 
-class Carpeta:
-    def __init__(self,nombre):
-        self._nombre = nombre
-        self._mensajes = []
 
-    def agregar_mensaje(self, mensaje):
-        self._mensajes.append(mensaje)
-
-    def eliminar_mensajes(self,mensaje):
-        self._mensajes.remove(mensaje)
-
-    def listar_mensajes(self):
-        return self._mensajes
-
-class ServidorCorreo:
-    def __init__(self, email):
-        self._email = email
-        self._usuarios = []
-
-    def agregar_usuario(self, usuario):
-        self._usuarios.append(usuario)
-
-    def eliminar_usuario(self, usuario):
-        self._usuarios.remove(usuario)
-
-    def obtener_usuarios(self):
-        return self._usuarios
+class Carpeta: # Se crea la carpeta correspondiente.
+    def __init__(self, nombre):
+        self.nombre = nombre # Recibe el nombre de la carpeta (inbox, enviados, etc)
+        self.mensajes = [] # Crea la lista vacia de mensajes
+    
+    def msjes(self):
+        return self._mensajes # Retorna todos los mensajes
+    
+    def agg_msjs(self, mensaje): # Se define la función y recibe el mensaje
+        return self._mensajes.append(mensaje) # Agrega el mensaje a la lista de mensajes
+    
+    def delete_msjs(self, mensaje): # Se define la función y se recibe el parametro del mensaje
+        if mensaje in self._mensajes: # Busqueda de msj en la lista de msjs
+            self._mensajes.remove(mensaje)  # Elimina msj de una lista
+    
+    def listar_msjs(self):
+        return self._mensajes # Recibe todos los mensajesew.
+    
