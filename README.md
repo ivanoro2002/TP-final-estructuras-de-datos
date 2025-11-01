63c99d8e9130cd5bacfb46866c7fafd36b557040
alexispoliti <walexispoliti@gmail.com>
2025-11-01 08:15:09 -0300

    add filtros

--- README.md
+++ README.md
@@ -22,12 +22,13 @@ El sistema simula el comportamiento de un servidor de correo y la interacción d
 3. Durante el primer día se estableció la base principal del codigo, este incluia las distintas clases a utilizar (GestionCorreo(interfaz), ServidorCorreo, Usuario, Mensaje, Carpeta).
 4. Posteriormente se implementaron los atributos y los distintos metodos para cada clase.
 5. Una vez concluida la estructuración del código se paso a la fase de elaboración del diagrama en base al codigo para poder visualizar la relación entre las clases.
+
 2º Entrega
-
-
 1. Se importó la clase Mensaje e implementó el arbol de carpetas dentro de la clase Carpeta que permite la adición de subcarpetas ("carpetas hijas").
 2. Se utilizó un metodo para mover mensajes entre carpetas y buscar mensajes recursivamente de manera tal que busque desde la primer carpeta hasta la ultima mediante filtros (asunto y correo del emisor)
 3. Se corrigieron los errores en los metodos recursivos, se cambio el parametro "mensaje" por "texto" para que sea más especifico y claro sobre lo que trabaja. 
 4. En la recursividad de buscar_msjs habia un error de accesos en la propiedad .asunto, .mensaje y .emisor, el cual ya fue solucionado eliminando los "__". 
 5. Considerando los casos limites, cuando el usuario ingrese un parametro que no es un string, el programa lanzará un error, esto se soluciono con la adición del metodo "isinstance" para que la busqueda funcione correctamente transformando aquellos None, numero (354, 2, 3), listas, etc, en cadenas de texto. Por ejemplo: (123) = ("123).
-6. Con respecto al movimiento de mensajes, implementamos un caso base para que el metodo empiece a recorrer las carpetas y sus subC con el fin de encontrar un mensaje. Por otro lado, para evitar errores se utilizo el metodo "isinstance" para verificar que un parametro corresponda con lo existencia de mensajes y/o carpetas. Esto hace que, si el usuario ingresa un mensaje o carpeta no válido, se le comunique el error. 
+6. Con respecto al movimiento de mensajes, implementamos un caso base para que el metodo empiece a recorrer las carpetas y sus subC con el fin de encontrar un mensaje. Por otro lado, para evitar errores se utilizo el metodo "isinstance" para verificar que un parametro corresponda con lo existencia de mensajes y/o carpetas. Esto hace que, si el usuario ingresa un mensaje o carpeta no válido, se le comunique el error.
+
+3º Entrega
--- entrega 2/usuario.py
+++ entrega 2/usuario.py
@@ -19,25 +19,63 @@ class GestionCorreo(ABC):
 class Usuario(GestionCorreo):
     def __init__(self, nombre, email, servidor, carpetas):
         self._nombre = nombre
-        self._email = email
+        self._mail = email
         self._servidor = servidor
-        self._carpetas = {
+        self.__carpetas = {
             "inbox": Carpeta("inbox"),
             "enviados": Carpeta("enviados"),
             "papelera": Carpeta("papelera")
         }    
+        self._filtros = {}
+
+    @property
+    def email(self):
+        return self._email
+    
+    @property
+    def nombre(self):
+        return self._nombre
 
     def enviar_msjs(self, destinatarios, asunto, contenido):
+        if not isinstance(asunto, str) or not isinstance(contenido, str): # Valida si el contenido de las claves sea del tipo correspondiente.
+            raise TypeError("El asunto y el contenido debe ser texto (strings).")
         mensaje = Mensaje(self._email, destinatarios, asunto, contenido)
-        self._carpetas["enviados"].agg_msjs(mensaje)
-        for user in self._servidor.obtener_usuarios():
-            if user._email in destinatarios:
-                user.recibir_msjs(mensaje)
+        self._carpetas["enviados"].agg_msjs(mensaje)  # A la carpeta "enviados", se le agrega el mensaje
+        for user in self._servidor.obtener_usuarios():  # A cada usuario en el servidor 
+            if user.email in destinatarios:  # Si el email del user es parte de los destinatarios
+                user.recibir_msjs(mensaje)     # Este recibe el msj
                 
-    def recibir_msjs(self, mensaje):
-        self._carpetas["inbox"].agg_msjs(mensaje)
+    def recibir_msjs(self, mensaje): 
+        if not isinstance(mensaje, Mensaje): # Valida que sea instancia de Mensaje. 
+            raise TypeError("Esto no es un mensaje.") 
+        dest = "inbox"  # Inicialmente los mensajes son recibidos en la carpeta "inbox" 
+        for p, c_dest in self._filtros.items():   # Toma la posiciones de palabra clave y carpeta en el dic de filtros
+            if p.lower() in mensaje.asunto.lower() or p.lower() in mensaje.emisor.lower():  # si el mensaje se encuentra contenida en "asunto" o "emisor"
+                dest = c_dest     # Lo asigna al filtro que corresponda
+                break     # Termina al eonctrar el primer filtro a aplicarse
+        self.__carpetas[dest].agg_msjs(mensaje) # Agg el mensaje a la carpeta que corresponda
 
     def listar_msjs(self, carpeta):
-        if carpeta in self._carpetas:
-            return self._carpetas[carpeta].listar_msjs()
-        return []
+        if carpeta in self._carpetas:   # Si el usuario quiere una carpeta y existe
+            return self._carpetas[carpeta].listar_msjs()   # Lista los mensajes de la carpeta seleccionada
+        return [] # Si no, no retorna nada.
+
+    def agg_palabras(self, p_clave, c_dest):  # Solicita palabra a filtrar y carpeta destino para la misma.
+        if c_dest not in self.__carpetas:   # Si la carpeta destino no está en las claves de carpetas.
+            raise ValueError("La carpeta", c_dest, "no existe.") 
+        self._filtros[p_clave.lower()] = c_dest # Si está, se agrega la palabra a la carpeta destino. (en minuscula)
+
+    def pr_filtros(self):
+        if not self._filtros:    # Si el usuario no agregò filtros
+            return "No hay filtros existentes."
+        else: 
+            for p, c in self._filtros.items():     # Recorre con dos variables y toma ambas con .items 
+                print(p, ":", c)         # Imprime palabra clave y carpeta
+
+
+    def elim_filtro(self, p_clave):
+        if p_clave in self._filtros:
+            del self._filtros[p_clave.lower()]
+        else:
+            print("El filtro de la palabra ingresada es inexistente.")
+
