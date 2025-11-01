38d96d51b05869e118937f5bb087ea3e4336c315
Ivan <valicentiivan@gmail.com>
2025-11-01 19:23:23 -0300

    subida 1/10

--- entrega 2/mensaje.py
+++ entrega 2/mensaje.py
@@ -1,10 +1,11 @@
 class Mensaje:  # Sea crea el mensaje 
-    def __init__(self, emisor, destinatarios, asunto, contenido):
+    def __init__(self, emisor, destinatarios, asunto, contenido,prioridad = 0):
         self.__emisor = emisor
         self.__destinatarios = destinatarios
         self.__asunto = asunto
         self.__contenido = contenido    
-      
+        self._prioridad = prioridad
+        self._leido = False
     @property
     def emisor(self):
         return self.__emisor
@@ -16,3 +17,10 @@ class Mensaje:  # Sea crea el mensaje
     @property
     def asunto(self):
         return self.__asunto
+
+    @property
+    def prioridad(self):
+        return self._prioridad
+
+    def marcar_urgente(self, level=1):
+        self._prioridad = level
--- entrega 2/usuario.py
+++ entrega 2/usuario.py
@@ -34,10 +34,26 @@ class Usuario(GestionCorreo):
             if user._email in destinatarios:
                 user.recibir_msjs(mensaje)
                 
-    def recibir_msjs(self, mensaje):
-        self._carpetas["inbox"].agg_msjs(mensaje)
+    def recibir_msjs(self, mensaje): 
+        if not isinstance(mensaje, Mensaje): # Valida que sea instancia de Mensaje. 
+            raise TypeError("Esto no es un mensaje.") 
+        dest = "inbox"  # Inicialmente los mensajes son recibidos en la carpeta "inbox" 
+        if "URGENTE".lower() in mensaje.asunto.lower():  # Si asunto contiene "urgente"
+            self._urgente.append(mensaje)      # Irá directo a la cola de urgentes
+        for p, c_dest in self._filtros.items():   # Toma la posiciones de palabra clave y carpeta en el dic de filtros
+            if p.lower() in mensaje.asunto.lower() or p.lower() in mensaje.emisor.lower():  # si el mensaje se encuentra contenida en "asunto" o "emisor"
+                dest = c_dest     # Lo asigna al filtro que corresponda
+                break     # Termina al eonctrar el primer filtro a aplicarse
+        self.__carpetas[dest].agg_msjs(mensaje) # Agg el mensaje a la carpeta que corresponda
+                
+
 
     def listar_msjs(self, carpeta):
-        if carpeta in self._carpetas:
-            return self._carpetas[carpeta].listar_msjs()
-        return []
+        if not isinstance(carpeta, str):
+            raise TypeError("La carpeta debe ser escrita con texto.")
+        if carpeta in self._carpetas:   # Si el usuario quiere una carpeta y existe
+            if self._urgente:          # Si no esta vacia 
+                for msj in self._urgente:
+                    print("URGENTE:", msj.asunto)   
+            return self._carpetas[carpeta].listar_msjs()   # Lista los mensajes de la carpeta seleccionada
+        return [] # Si no, no retorna nada.
